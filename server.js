cfe6d2de82b606e9ddcc57d9c6e9c6427c01d29b
oklukaan <kaan.oklu@hotmail.com>
2023-03-21 17:05:37 +0300

    qwe

--- server.js
+++ server.js
@@ -42,7 +42,7 @@ app.use(cors());
 
   
 app.use(function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Origin', 'https://mychatclient.vercel.app/');
     next();
   });
 
